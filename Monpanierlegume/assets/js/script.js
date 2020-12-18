511f44f0a62007736004edfc96ea0555b25e2117
Wendy <74055268+Lunalya@users.noreply.github.com>
2020-12-18 16:32:51 +0100

    Add files via upload

---
+++ Monpanierlegume/assets/js/script.js
@@ -0,0 +1,35 @@
+let togg1 = document.getElementById("togg1");
+let togg2 = document.getElementById("togg2");
+let d1 = document.getElementById("d1");
+let d2 = document.getElementById("d2");
+togg1.addEventListener("click", () => {
+  if(getComputedStyle(d1).display != "none"){
+    d1.style.display = "none";
+  } else {
+    d1.style.display = "block";
+  }
+})
+
+togg2.addEventListener("click", () => {
+  if(getComputedStyle(d2).display != "none"){
+    d2.style.display = "none";
+  } else {
+    d2.style.display = "block";
+  }
+})
+
+togg3.addEventListener("click", () => {
+  if(getComputedStyle(d3).display != "none"){
+    d3.style.display = "none";
+  } else {
+    d3.style.display = "block";
+  }
+})
+
+togg4.addEventListener("click", () => {
+  if(getComputedStyle(d4).display != "none"){
+    d4.style.display = "none";
+  } else {
+    d4.style.display = "block";
+  }
+})
